9cc3264f84970eddffa60fe366f5c15be5d87205
budougumi0617 <budougumi0617@gmail.com>
2017-07-04 01:46:18 +0900

    Recreate by VS2017

---
+++ Testable.Tests/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Testable.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            var c = new Class1();
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
