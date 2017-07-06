920a173cd397cd49db2093c2143d7566439e0e64
budougumi0617 <budougumi0617@gmail.com>
2017-07-06 09:30:34 +0900

    :+1: Add function declarations.

---
+++ Testable/PrivateObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Testable
+{
+	/// <summary>
+	/// This is clone of Microsoft.VisualStudio.TestTools.UnitTesting
+	/// </summary>
+	/// <seealso cref="https://msdn.microsoft.com/ja-jp/library/microsoft.visualstudio.testtools.unittesting.privateobject.aspx"/>
+	public class PrivateObject
+    {
+		/// <summary>
+		/// Initializes a new instance of the PrivateObject class that creates the wrapper for the specified object.
+		/// </summary>
+		/// <param name="obj">The object to wrap.</param>
+		public PrivateObject(Object obj)
+        {
+            
+        }
+
+		/// <summary>
+		/// Gets a value from a named field, based on the name.
+		/// </summary>
+		/// <param name="name">The name of the private field to get.</param>
+		/// <returns>The value set for the name field.</returns>
+		public object GetField(string name){
+            return null;
+        }
+    }
+}
